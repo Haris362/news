c73054b95f4fa053070fc738c95eb365110f59ad
Haris362 <haris.tariq423@gmail.com>
2025-07-16 15:33:08 +0500

    adding country name feature

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const news = async function () {
     const key = `f3eabcc900f956dd2972aad01bfe5b90`
-    const country = `pk`;
+    
+
     try {
-        const response = await fetch(`https://gnews.io/api/v4/search?q=pakistan&country=${country}&lang=en&max=10&token=${key}`);
+        const country = document.getElementById("input");
+        document.getElementById("search").addEventListener("click",async function(){
+        const val = country.value.toLowerCase().trim()
+        const response = await fetch(`https://gnews.io/api/v4/top-headlines?country=${val}&lang=en&max=10&token=${key}`);
         const data = await response.json()
         const newsData = data.articles;
         console.log(data);
 
         const newsContainer = document.getElementById("news-Container");
         newsData.forEach(element => {
+
+         
             const card = document.createElement("div");
             card.classList.add("news-card")
             card.innerHTML = `
-                <img src="${element.image}" alt="">
+                
                 <div class="news-content">
                 <h3>${element.title}</h3>
                 <p>${element.description}</p>
@@ -21,6 +27,7 @@ const news = async function () {
         `
             newsContainer.appendChild(card)
         });
+        })
     } catch (error) {
         console.log(error);
 
